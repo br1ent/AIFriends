f5e66daf12ca017580ffa88ee555c9811a00f7d8
石铭轩 <530220399@qq.com>
2026-04-25 09:56:41 +0800

    创建导航栏

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,13 @@
 <template>
-
+  <NavBar>
+    页面内容
+  </NavBar>
 </template>
 
 <script setup>
+import NavBar from "@/components/navbar/NavBar.vue";
 
 </script>
 
-<style>
-
-
+<style scoped>
 </style>
