2d51655a7c0c81fd740050009ef0bc38344fdd83
石铭轩 <530220399@qq.com>
2026-04-27 09:44:49 +0800

    实现更新用户api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -6,6 +6,7 @@ from web.views.user.account.login import LoginView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
 from web.views.user.account.logout import LogoutView
+from web.views.user.profile.update import UpdateProfileView
 
 urlpatterns = [
     path("api/user/account/login/", LoginView.as_view(), name="login"),
@@ -13,6 +14,8 @@ urlpatterns = [
     path("api/user/account/register/", RegisterView.as_view(), name="register"),
     path("api/user/account/refresh_token/", RefreshTokenView.as_view(), name="refresh_token"),
     path("api/user/account/get_user_info/", GetUserInfoView.as_view(), name="get_user_info"),
+
+    path("api/user/profile/update/", UpdateProfileView.as_view(), name="update_profile"),
     path("", index, name="index"),
 
     re_path(r'^(?!media/|static/|assets/).*$', index)
